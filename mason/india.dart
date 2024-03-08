9cd3139712662d7b3d51eb992aef4dd11120035b
Dhinesh J <dhinesh.jeyasankar.ext@ysil.in>
2024-03-08 12:25:47 +0530

    mason init

---
+++ mason/india.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+
+
+class India extends StatefulWidget {
+  const India({Key? key}) : super(key: key);
+
+  @override
+  State<India> createState() => _IndiaState();
+}
+
+class _IndiaState extends State<India> {
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+      appBar: AppBar(title: Text("india",),),
+    );
+  }
+}
+
